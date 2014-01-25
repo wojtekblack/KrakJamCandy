696c495aecf683c187100539957d1d622c100675
Filip <fblost@gmail.com>
2014-01-25 03:43:12 +0100

    Szkielet klasy dot

--- KrakJam2013/GameScreen.cpp
+++ KrakJam2013/GameScreen.cpp
@@ -1,5 +1,10 @@
 #include "pch.hpp"
+#include "Dot.hpp"
 #include "GameScreen.hpp"
+#include <sstream>
+#include <string> 
+#include <windows.h>
+#include <iostream>
 
 void GameScreen::Load(void)
 {
@@ -15,6 +20,8 @@ void GameScreen::Init(void)
     sampleText.setColor(sf::Color::Red);
     sampleText.setStyle(sf::Text::Bold | sf::Text::Underlined);
     sampleText.setPosition(10.0f, 10.0f);
+
+	generateDots(5);
 }
 
 void GameScreen::HandleEvent(sf::Event const & event)
@@ -32,4 +39,25 @@ void GameScreen::Render(void)
 
 void GameScreen::Unload(void)
 {
+}
+
+void GameScreen::generateDots(int number)
+{	
+	for(int i=0; i<dots.size(); i++)
+		delete dots[i];
+	dots.clear();
+
+	for(int i=0; i < number; i++){
+		Dot *d = new Dot;
+		d->init();
+		dots.push_back(d);
+	}
+}
+
+void GameScreen::debugPrint(std::stringstream stream)
+{
+	std::string str;
+	std::getline(stream, str);
+
+	OutputDebugStringA(str.c_str());
 }
--- KrakJam2013/GameScreen.hpp
+++ KrakJam2013/GameScreen.hpp
@@ -1,12 +1,25 @@
 #pragma once
 
 #include "Screen.hpp"
+#include <string>
+
+class Dot;
+
+enum GameColours
+{
+	RED = 0,
+	GREEN,
+	YELLOW,
+	GameColoursSIZE
+};
 
 class GameScreen : public Screen
 {
     private:
     sf::Text sampleText;
     sf::Font arialFont;
+
+	std::vector<Dot*> dots;
 
     public:
     void Load(void);
@@ -15,4 +28,8 @@ class GameScreen : public Screen
     void Update(void);
     void Render(void);
     void Unload(void);
+
+	void generateDots(int);
+
+	static void debugPrint(std::stringstream);
 };
